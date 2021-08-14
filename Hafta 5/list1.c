51cc5ac289ff22462e5ec33a0e61b94e465ffa34
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2021-08-14 17:12:01 +0300

    Add files via upload

---
+++ Hafta 5/list1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int list[3];
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+    for(int i = 0; i < 3; i++)
+    {
+        printf("%i\n",list[i]);
+        
+    }
+}
